507ac13973ace78a4afbdb62681777f90bf69639
Spenser Solys <solysspenser@gmail.com>
2016-03-09 01:11:41 -0500

    Starting over
    Added a couple fun things

---
+++ Assets/Scripts/generateObjects.cs
@@ -0,0 +1,92 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Text;
+using System.IO;
+using System.Collections;
+using System.Collections.Generic;
+using System;
+
+public class generateObjects : MonoBehaviour {
+	
+	void Start () {
+
+		readFromFile rff = new readFromFile();
+		List<string> armor = rff.Load(Application.dataPath + "/zFiles/armorList.txt");
+
+		Debug.Log(armor[0]);
+	
+		for(int layerOne = 0; layerOne < 5; layerOne++) {
+			GameObject layerOneObj = new GameObject();
+			layerOneObj.transform.SetParent(this.transform);
+			layerOneObj.name = "layerOneItem" + layerOne;
+			layerOneObj.AddComponent<SpriteRenderer>();
+			//layerOneItem.AddComponent<MenuObject>();
+
+			GameObject hoverObj = new GameObject();
+			hoverObj.transform.SetParent(layerOneObj.transform);
+			hoverObj.name = "hoverArea";
+			//hoverObj.AddComponent<HoverArea>();
+
+			GameObject pressObj = new GameObject();
+			pressObj.transform.SetParent(layerOneObj.transform);
+			pressObj.name = "pressArea";
+			//pressObj.AddComponent<PressArea>();
+
+		}
+
+		for(int boxItem = 0; boxItem < 20; boxItem++) {
+			// Set up the base object
+			GameObject boxItemObj = new GameObject();
+			boxItemObj.transform.SetParent(this.transform);
+			boxItemObj.name = "boxItem" + boxItem;
+			boxItemObj.AddComponent<SpriteRenderer>();
+			//boxItemObj.AddComponent<MenuObject>();
+		
+			// Setup the hover child
+			GameObject hoverObj = new GameObject();
+			hoverObj.transform.SetParent(boxItemObj.transform);
+			hoverObj.name = "hoverArea";
+			//hoverObj.AddComponent<HoverArea>();
+
+			// Setup the press child
+			GameObject pressObj = new GameObject();
+			pressObj.transform.SetParent(boxItemObj.transform);
+			pressObj.name = "pressArea";
+			//pressObj.AddComponent<PressArea>();
+		
+			// Setup the text child
+			GameObject textHolder = new GameObject();
+			textHolder.transform.SetParent(boxItemObj.transform);
+			textHolder.name = "textHolder";
+			textHolder.AddComponent<TextMesh>();
+
+			// Setup the icon child
+			GameObject iconHolder = new GameObject();
+			iconHolder.transform.SetParent(boxItemObj.transform);
+			iconHolder.name = "iconHolder";
+			iconHolder.AddComponent<SpriteRenderer>();	
+
+		}
+
+		for(int smallCircle = 0; smallCircle < 20; smallCircle++) {
+			GameObject smallCircleObj = new GameObject();
+			smallCircleObj.transform.SetParent(this.transform);
+			smallCircleObj.name = "smallCircle" + smallCircle;
+			smallCircleObj.AddComponent<SpriteRenderer>();
+			//layerOneItem.AddComponent<MenuObject>();
+			
+			GameObject hoverObj = new GameObject();
+			hoverObj.transform.SetParent(smallCircleObj.transform);
+			hoverObj.name = "hoverArea";
+			//hoverObj.AddComponent<HoverArea>();
+			
+			GameObject pressObj = new GameObject();
+			pressObj.transform.SetParent(smallCircleObj.transform);
+			pressObj.name = "pressArea";
+			//pressObj.AddComponent<PressArea>();
+		}
+
+	}
+
+
+}
---
+++ Assets/Scripts/readFromFile.cs
@@ -0,0 +1,31 @@
+﻿using System.Text;
+using System.IO;
+using System.Collections;
+using System.Collections.Generic;
+using System;
+
+public class readFromFile {
+
+	public List<string> results = new List<string>();
+
+	public List<string> Load(string fileName) {
+		try {
+			string line;
+			StreamReader theReader = new StreamReader(fileName, Encoding.Default);
+			using (theReader) {
+				do {
+					line = theReader.ReadLine();
+					if (line != null && line != "----------") {
+						results.Add(line);
+					}
+				}
+				while (line != null);
+				theReader.Close();
+				return results;
+			}
+		} catch (Exception e) {
+			//Console.WriteLine("{0}\n", e.Message);
+			return null;
+		}
+	}
+}
