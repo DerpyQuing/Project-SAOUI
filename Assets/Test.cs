cf1ca1d21ab58d5bb631630f9f6c7ee9ef15fabc
Spenser Solys <solysspenser@gmail.com>
2016-03-11 02:29:24 -0500

    Groundwork
    If you don't eat your meat, you can't have any pudding

--- Assets/Test.cs
+++ Assets/Test.cs
@@ -12,11 +12,17 @@ public class Test : MonoBehaviour {
 		//JsonMapper.ToObject()
 		//JsonData data = JsonMapper.ToObject(rff.LoadJSONResourceFile("ReferenceFiles/menu.json"));
 		//Debug.Log((string)data["Menu"]);
-		PrintJson(rff.LoadJSONResourceFile("ReferenceFiles/menu.json"));
+		PrintJson(rff.LoadJSONResourceFile("ReferenceFiles/test.json"));
 	}
 	
 	public static void PrintJson(string json) {
 		JsonReader reader = new JsonReader(json);
+		JsonData data = JsonMapper.ToObject(reader);
+
+		Debug.Log(data["name"]);
+		Debug.Log(data[0]);
+	
+	
 
 		// The Read() method returns false when there's nothing else to read
 		while (reader.Read()) {
