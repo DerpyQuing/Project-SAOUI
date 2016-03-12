0ac4e731970df8ee17a1d405ffdfc5b122905940
Spenser Solys <solysspenser@gmail.com>
2016-03-12 03:22:21 -0500

    Decided on .json format
    Added a test method to recursively iterate through the json menu
    structure. Now to create menu objects based of the JSONNode of each
    iteration

--- Assets/Scripts/Test.cs
+++ Assets/Scripts/Test.cs
@@ -4,6 +4,8 @@ using SimpleJSON;
 
 public class Test : MonoBehaviour {
 
+	public GameObject tcube;
+
 	// Use this for initialization
 	void Start () {
 		readFromFile rff = new readFromFile();
@@ -13,25 +15,70 @@ public class Test : MonoBehaviour {
 		//JsonData data = JsonMapper.ToObject(rff.LoadJSONResourceFile("ReferenceFiles/menu.json"));
 		//Debug.Log((string)data["Menu"]);
 		//PrintJson(rff.LoadJSONResourceFile("ReferenceFiles/test.json"));
-		jk ();
+
+		//createTestItems ();
+		maine();
 	}
 	
 
 
 
-	public void jk () {
+	public void test () {
 		readFromFile rff = new readFromFile();
 		string json = rff.LoadJSONResourceFile("ReferenceFiles/test.json");
 
 		JSONNode j = JSONNode.Parse(json);
-		foreach (string k in j["Menu"]["Player"]["Equipment"]["Weapons"]["Simple Sword"].Keys){
-			Debug.Log (k);
-			Debug.Log(j["Menu"]["Player"]["Equipment"]["Weapons"]["Simple Sword"][k].AsInt);
-		}
 
+
+		for(int i = 0; i < j["Menu"].Count; i++) {
+			Debug.Log(j["Menu"][i]["_baseIconPath"].ToString());
+			Debug.Log(j["Menu"][i]["_activeIconPath"].ToString());
+			Debug.Log(j["Menu"][i]["_text"].ToString());
+			Debug.Log(j["Menu"][i]["_hasChildren"].AsBool);
+			Debug.Log("-------------------------");
+		}
 
 	}
 
+	public void createTestItems() {
+		readFromFile rff = new readFromFile();
+		string json = rff.LoadJSONResourceFile("ReferenceFiles/test.json");
+		JSONNode j = JSONNode.Parse(json);
+		JSONNode k = j["Menu"];
+
+		for(int i = 0; i < j["Menu"].Count; i++) {
+			GameObject.Instantiate(tcube);
+			if(j["Menu"][i]["_hasChildren"].AsBool) {
+
+			}
+		}
+	}
+
+
+
+	public void maine() {
+		readFromFile rff = new readFromFile();
+		string menu = rff.LoadJSONResourceFile("ReferenceFiles/test.json");
+		JSONNode jsonNode = JSONNode.Parse(menu);
+		testRecursizeness(jsonNode["Menu"]);
+	}
+
+	// To quote the infamous natty-ice, "Fuck the club up,"
+	public bool testRecursizeness(JSONNode jsonNode) {
+		for(int i = 0; i < jsonNode.Count; i++) {
+			if(jsonNode[i]["_hasChildren"] != null) {
+				createTestItems (jsonNode[i]);
+				if(jsonNode[i]["_hasChildren"].AsBool) {
+					testRecursizeness(jsonNode[i]); 
+				}
+			}
+		}
+		return false;
+	}
+
+	public void createTestItems (JSONNode jsonNode) {
+		Debug.Log(jsonNode["_text"]);
+	}
 
 
 
